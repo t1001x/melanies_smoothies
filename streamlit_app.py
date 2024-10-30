e5a7dfa50b5c8f0e0a8290cc31de0f3f841223c7
t1001x <bluesky1001@wp.pl>
2024-10-30 15:37:15 +0100

    Update streamlit_app.py
    connection string update

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import streamlit as st
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your smoothie :cup_with_straw:")
